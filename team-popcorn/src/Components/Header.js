a321dfe682ce7057644b1b97b9bf23178123b5d4
tullinge1996 <31662409+tullinge1996@users.noreply.github.com>
2018-04-24 16:13:39 +0200

    Added Navbar icons

--- team-popcorn/src/Components/Header.js
+++ team-popcorn/src/Components/Header.js
@@ -5,11 +5,25 @@ import "../App.css";
 
 class Header extends Component {
   render(){
+    var iconStyle={
+      paddingLeft:"10px",
+      paddingTop:"12px",
+      paddingRight:"3px",
+
+    }
+    var iconStyle2={
+      paddingLeft:"15px",
+      paddingTop:"12px",
+      paddingRight:"3px",
+
+    }
     return(
 <div className ="HeaderContainer">
-<p>Icon</p>
-<p>Icon</p>
-<p>Icon</p>
+<div>
+<i className="fab fa-google fa-lg"  style={iconStyle}></i>
+<i className="fas fa-bell fa-lg" style={iconStyle2}></i>
+<i className="fas fa-info-circle fa-lg"style={iconStyle2}></i>
+</div>
 </div>
     );
   }
--- team-popcorn/src/Components/SideBar.js
+++ team-popcorn/src/Components/SideBar.js
@@ -5,11 +5,21 @@ import "../App.css";
 
 class SideBar extends Component {
   render(){
+    var iconStyle={
+      paddingTop:"5px",
+      paddingBottom:"12px",
+
+    }
+    var iconStyle2={
+      paddingLeft:"15px",
+      paddingTop:"12px",
+      paddingRight:"3px",
+
+    }
     return(
 <div className ="SideBarContainer">
-<p>Icon</p>
-<p>Icon</p>
-<p className ="ExitButton">Icon</p>
+<i className="far fa-chart-bar fa-lg"  style={iconStyle}></i>
+<i className="far fa-money-bill-alt fa-lg" style={iconStyle}></i>
 </div>
     );
   }
