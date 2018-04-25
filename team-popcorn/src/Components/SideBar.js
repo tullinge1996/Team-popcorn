4b67f433845922267b6cfa870709f76c61d44d97
tullinge1996 <31662409+tullinge1996@users.noreply.github.com>
2018-04-25 17:20:55 +0200

    added expandable sidebar

--- team-popcorn/src/Components/SideBar.js
+++ team-popcorn/src/Components/SideBar.js
@@ -6,8 +6,22 @@ class SideBar extends Component {
   render(){
     return(
 <div className ="SideBarContainer">
-<i  id ="iconStyle3" className="far fa-chart-bar fa-lg"></i>
+  <ul>
+<li><a href="#"><i  id ="iconStyle3" className="far fa-chart-bar fa-lg"></i></a>
+<ul className ="secondUl">
+<li>Text</li>
+<li>Text</li>
+<li>Text</li>
+<li>Text</li>
+<li>Text</li>
+<li>Text</li>
+<li>Text</li>
+</ul>
+
+</li>
+</ul>
 <i  id ="iconStyle3" className="far fa-money-bill-alt fa-lg"></i>
+
 </div>
     );
   }
