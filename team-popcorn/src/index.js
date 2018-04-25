84399a2f810293e560e72cbce7a2f2ea335f0dd8
Viktor <viktor-stenqvist@hotmail.com>
2018-04-25 10:33:03 +0200

    fixed error

--- team-popcorn/src/index.js
+++ team-popcorn/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Navbar from './components/navbar.js'
-import Sidebar from './components/sidebar.js'
 import Menuparent from './components/menuParent.js'
-import Viewparent from './components/viewParent.js'
+
 
 ReactDOM.render(
   <div className ="root">
