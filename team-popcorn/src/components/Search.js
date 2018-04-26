2d623a71982943e9e590e6fa902865180b3ecba0
kyhrikard <rikard.carlsson@student.kyh.se>
2018-04-26 17:06:22 +0200

    Add search component again

---
+++ team-popcorn/src/components/Search.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import '../styles/search.css'
+
+export default class Search extends Component {
+    render() {
+        return (
+            <div className="search">
+                Search
+            </div>
+        )
+    }
+}
