294ba99f55923fadece01fbe86ac3c6b7f3f682a
kyhrikard <rikard.carlsson@student.kyh.se>
2018-04-26 13:34:19 +0200

    Add some styling

--- team-popcorn/src/components/Content.js
+++ team-popcorn/src/components/Content.js
@@ -6,12 +6,12 @@ export default class Content extends Component {
     render() {
         return (
             <div className="content">
-                <ShortcutItem />
-                <ShortcutItem />
-                <ShortcutItem />
-                <ShortcutItem />
-                <ShortcutItem />
-                <ShortcutItem />
+                <ShortcutItem h3="Finanial Analytics" h2="Sales Overview" />
+                <ShortcutItem h3="Product Analytics" h2="Sales Funnel" />
+                <ShortcutItem h3="Product Analytics" h2="Product Inventory" />
+                <ShortcutItem h3="Price Analytics" h2="Optimisation" />
+                <ShortcutItem h3="Data Queries" h2="Create Data Queries" />
+                <ShortcutItem h3="Data Segments" h2="Create Data Segment Groups" />
             </div>
         )
     }
--- team-popcorn/src/components/ShortcutItem.js
+++ team-popcorn/src/components/ShortcutItem.js
@@ -4,9 +4,15 @@ import '../styles/shortcutitem.css'
 export default class ShortcutItem extends Component {
     render() {
         return (
-            <div className="short-cut-item">
-                ShortcutItem
+            <div onClick={this.clickedMe} className="short-cut-item">
+                <h3>{this.props.h3}</h3>
+                <h2>{this.props.h2}</h2>
             </div>
         )
     }
+
+    clickedMe = () => {
+        alert(`You clicked ${this.props.h2}`)
+    }
 }
+
